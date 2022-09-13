96ecc3b89e3a349510aded113d81d47f9af493d8
Fabio Ferrari <fabioferrari@gmail.com>
2022-09-13 09:55:06 -0300

    corrige espaço

--- preset1.py
+++ preset1.py
@@ -1,2 +1,2 @@
 import urllib.request
-urllib.request.urlopen("http://localhost:7777/api/neoid/Preset 1").read()
+urllib.request.urlopen("http://localhost:7777/api/neoid/Preset%201").read()
--- preset2.py
+++ preset2.py
@@ -1,2 +1,2 @@
 import urllib.request
-urllib.request.urlopen("http://localhost:7777/api/neoid/Preset 2").read()
+urllib.request.urlopen("http://localhost:7777/api/neoid/Preset%202").read()
